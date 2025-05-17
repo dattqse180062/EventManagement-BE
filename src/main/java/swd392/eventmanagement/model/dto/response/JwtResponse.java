4c2f65fd49a581e17fbde23ee269ca334e73ff4c
dattqse180062 <dattqse180062@fpt.edu.vn>
2025-05-17 10:12:00 +0700

    Add Roles to JwtResponse

--- src/main/java/swd392/eventmanagement/model/dto/response/JwtResponse.java
+++ src/main/java/swd392/eventmanagement/model/dto/response/JwtResponse.java
@@ -1,6 +1,7 @@
 package swd392.eventmanagement.model.dto.response;
 
 import lombok.Data;
+import java.util.Set;
 
 @Data
 public class JwtResponse {
@@ -10,12 +11,14 @@ public class JwtResponse {
     private Long id;
     private String email;
     private String fullName;
+    private Set<String> roles;
     
-    public JwtResponse(String token, String refreshToken, Long id, String email, String fullName) {
+    public JwtResponse(String token, String refreshToken, Long id, String email, String fullName, Set<String> roles) {
         this.token = token;
         this.refreshToken = refreshToken;
         this.id = id;
         this.email = email;
         this.fullName = fullName;
+        this.roles = roles;
     }
 } 
