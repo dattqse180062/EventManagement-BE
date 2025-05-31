dd43f659db987099ad2630001cbe457d91d4db6b
dattqse180062 <dattqse180062@fpt.edu.vn>
2025-06-01 01:21:23 +0700

    Finish feature/get-event-by-category

--- src/main/java/swd392/eventmanagement/model/entity/EventCapacity.java
+++ src/main/java/swd392/eventmanagement/model/entity/EventCapacity.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "event_capacity", uniqueConstraints = {
